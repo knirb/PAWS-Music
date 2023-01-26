8cb1439055a60497787bc41b25266851d793d507
WalentinW <walentin.widgren@umain.com>
2023-01-26 20:51:55 +0100

    Add spotify routes  🚏

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,20 +1,21 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response, render_template
+from flask import Flask, Response, request, render_template
 import playlist
 import songs
 from helloworld.flaskrun import flaskrun
 import awscontroller
+import spotifycontroller
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def get():
-    return Response(json.dumps({'Output': 'Hello Worldddd'}), mimetype='application/json', status=200)
+# @app.route('/', methods=['GET'])
+# def get():
+#     return Response(json.dumps({'Output': 'Hello Worldddd'}), mimetype='application/json', status=200)
 
-@app.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200) 
+# @app.route('/', methods=['POST'])
+# def post():
+#     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200) 
 
 @app.route('/', methods=['GET'])
 def home():
@@ -31,6 +32,19 @@ app.register_blueprint(songs.bp)
 def get_songs():
     return awscontroller.get_songs()
 
+@app.route('/get-playlists', methods=['GET'])
+def get_playlists():
+    return spotifycontroller.get_playlists()    
+
+@app.route('/get-song', methods=['POST'])
+def get_song():
+    search_term = request.form['song_name']
+
+    return spotifycontroller.get_song(search_term)
+
+@app.route('/form')
+def renderform():
+    return render_template('form.html')            
 
 if __name__ == '__main__':
     flaskrun(app)
