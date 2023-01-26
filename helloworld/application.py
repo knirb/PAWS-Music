a882971e057188404e939a7a3909049524937146
Viktor Åsbrink <viktor@kreationsbyran.se>
2023-01-26 17:12:12 +0100

    add playlists route👏

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,23 +1,26 @@
 #!flask/bin/python
 import json
 from flask import Flask, Response
+import playlist
 from helloworld.flaskrun import flaskrun
 import awscontroller
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello Worldddd'}), mimetype='application/json', status=200)
 
-@application.route('/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
-@application.route('/get-songs', methods=['GET'])
+app.register_blueprint(playlist.bp)
+
+@app.route('/get-songs', methods=['GET'])
 def get_songs():
     return awscontroller.get_songs()
 
 
 if __name__ == '__main__':
-    flaskrun(application)
+    flaskrun(app)
