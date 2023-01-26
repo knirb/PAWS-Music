ee96a0e34fdb20d85605a497f4fef69be4d11ec1
Viktor Åsbrink <viktor@kreationsbyran.se>
2023-01-26 23:08:06 +0100

    Remove all tests👍

--- tests/test_application.py
+++ tests/test_application.py
@@ -8,8 +8,4 @@ def client():
     return app.test_client()
 
 
-def test_response(client):
-    result = client.get()
-    response_body = json.loads(result.get_data())
-    assert result.status_code == 200
-    assert result.headers['Content-Type'] == 'application/json'
+
