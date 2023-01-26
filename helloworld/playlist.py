39bc60fca7f6102bf88e9f4c64ed63b687908225
Fanny Rosén <fanny.rosen@umain.com>
2023-01-26 22:36:18 +0100

    added styling

--- helloworld/playlist.py
+++ helloworld/playlist.py
@@ -2,11 +2,8 @@ from flask import Blueprint, render_template
 import awscontroller
 bp = Blueprint('playlist', __name__, url_prefix='/playlist')
 
-    
-
 @bp.route('/', methods=['GET'])
 def home():
     res = awscontroller.get_playlists()
     return render_template('playlist/index.html', res = res)
-
 
